eb48c3cc8efc1400cac0548b75efac1aed8cb9d2
Welberrr <welberhenrique.costa@gmail.com>
2025-07-28 15:05:57 -0300

    Feat: Mapeando entidades JPA

---
+++ src/main/java/io/github/welberrr/libraryapi/model/Autor.java
@@ -0,0 +1,29 @@
+package io.github.welberrr.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name="autor", schema = "public")
+public class Autor {
+
+    @Id
+    @Column (name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", length = 50, nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+}
