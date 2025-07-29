7e05f646f870782a2b31643a270639feb8b78ad3
Welberrr <welberhenrique.costa@gmail.com>
2025-07-28 22:13:39 -0300

    Feat: Mapeando a entidade Livro

---
+++ src/main/java/io/github/welberrr/libraryapi/model/GeneroLivro.java
@@ -0,0 +1,5 @@
+package io.github.welberrr.libraryapi.model;
+
+public enum GeneroLivro {
+    FICCAO, FANTASIA, MISTERIO, ROMANCE, BIOGRAFIA, CIENCIA
+}
---
+++ src/main/java/io/github/welberrr/libraryapi/model/Livro.java
@@ -0,0 +1,40 @@
+package io.github.welberrr.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Data
+public class Livro {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao")
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "id_autor")
+    private Autor autor;
+}
