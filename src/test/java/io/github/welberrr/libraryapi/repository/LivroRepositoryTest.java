ee066f28cbd3f90c8b05a16724cd65ee8f7408e7
Welberrr <welberhenrique.costa@gmail.com>
2025-08-05 21:54:56 -0300

    Feat: Relacionamento - Entidade com Livro e Autor

---
+++ src/test/java/io/github/welberrr/libraryapi/repository/LivroRepositoryTest.java
@@ -0,0 +1,39 @@
+package io.github.welberrr.libraryapi.repository;
+
+import io.github.welberrr.libraryapi.model.Autor;
+import io.github.welberrr.libraryapi.model.GeneroLivro;
+import io.github.welberrr.libraryapi.model.Livro;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class LivroRepositoryTest {
+
+    @Autowired
+    LivroRepository repository;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Test
+    void salvarTest(){
+        Livro livro = new Livro();
+
+        livro.setIsbn("12345");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("UFO");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = autorRepository.findById(UUID.fromString(";aslkdjf;aslkjf")).orElse(null);
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
+    }
+}
