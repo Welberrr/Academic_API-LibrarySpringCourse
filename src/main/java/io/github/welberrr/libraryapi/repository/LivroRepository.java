f0d7963ea6f53fb4c2346c223972e0d43b8bca28
Welberrr <welberhenrique.costa@gmail.com>
2025-08-15 23:28:29 -0300

    Feat: Metodos de busca declarativos

--- src/main/java/io/github/welberrr/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/welberrr/libraryapi/repository/LivroRepository.java
@@ -4,6 +4,7 @@ import io.github.welberrr.libraryapi.model.Autor;
 import io.github.welberrr.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,4 +13,6 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
     List<Livro> findByAuto(Autor autor);
     List<Livro> findByTitulo(String titulo);
     List<Livro> findByISBN(String titulo);
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
+    List<Livro> findByTituloLike(String titulo);
 }
