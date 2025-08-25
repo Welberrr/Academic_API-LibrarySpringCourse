cab54633636697fa4cd568faa7ec16a43f01e274
Welberrr <welberhenrique.costa@gmail.com>
2025-08-25 20:22:31 -0300

    Feat: Adicionando Auditoria JPA nas entidades

--- src/main/java/io/github/welberrr/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/welberrr/libraryapi/LibraryapiApplication.java
@@ -1,29 +1,13 @@
 package io.github.welberrr.libraryapi;
 
-import io.github.welberrr.libraryapi.model.Autor;
-import io.github.welberrr.libraryapi.repository.AutorRepository;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(LibraryapiApplication.class, args);
-		AutorRepository repository = context.getBean(AutorRepository.class);
-
-		exemploSalvarRegistro(repository);
 	}
 
-	public static void exemploSalvarRegistro(@org.jetbrains.annotations.NotNull AutorRepository autorRepository){
-		Autor autor = new Autor();
-		autor.setNome("Welber");
-		autor.setNacionalidade("Brasileiro");
-		autor.setDataNascimento(LocalDate.of(2004, 9, 25));
-
-		var autorSalvo = autorRepository.save(autor);
-		System.out.printf("Autor Salvo: " + autorSalvo);
-	}
 }
