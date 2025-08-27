80af0084647bdbdd02991f7c5515c83831f95242
Welberrr <welberhenrique.costa@gmail.com>
2025-08-27 15:38:05 -0300

    Feat: Obtendo detalhes do autor

--- src/main/java/io/github/welberrr/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/welberrr/libraryapi/controller/dto/AutorDTO.java
@@ -4,7 +4,7 @@ import io.github.welberrr.libraryapi.model.Autor;
 
 import java.time.LocalDate;
 
-public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
+public record AutorDTO(java.util.UUID id, String nome, LocalDate dataNascimento, String nacionalidade) {
 
     public Autor mapearParaAutor(){
         Autor autor = new Autor();
