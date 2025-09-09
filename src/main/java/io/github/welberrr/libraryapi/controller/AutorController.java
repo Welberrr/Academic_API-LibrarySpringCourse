a8cbf5f4d538ca7191863d0124ee3de39ae0dd06
Welberrr <welberhenrique.costa@gmail.com>
2025-09-08 21:01:22 -0300

    Feat: Facilitando a injeção com RequiredArgsConstructor - Lombok

--- src/main/java/io/github/welberrr/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/welberrr/libraryapi/controller/AutorController.java
@@ -6,6 +6,7 @@ import io.github.welberrr.libraryapi.exceptions.OperacaoNaoPermitidaEception;
 import io.github.welberrr.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.welberrr.libraryapi.model.Autor;
 import io.github.welberrr.libraryapi.service.AutorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,14 +20,10 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
-
+@RequiredArgsConstructor
 public class AutorController {
 
     private final AutorService service;
-
-    public AutorController(AutorService service){
-        this.service = service;
-    }
 
     @PostMapping
     public ResponseEntity <Object> salvar(@RequestBody AutorDTO autor) {
--- src/main/java/io/github/welberrr/libraryapi/service/AutorService.java
+++ src/main/java/io/github/welberrr/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import io.github.welberrr.libraryapi.model.Autor;
 import io.github.welberrr.libraryapi.repository.AutorRepository;
 import io.github.welberrr.libraryapi.repository.LivroRepository;
 import io.github.welberrr.libraryapi.validator.AutorValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +13,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+
+@RequiredArgsConstructor
 public class AutorService {
 
     private final AutorRepository repository;
     private final AutorValidator validator;
     private final LivroRepository livroRepository;
-
-    public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository){
-        this.repository = repository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
 
     public Autor salvar(Autor autor){
         validator.validar(autor);
