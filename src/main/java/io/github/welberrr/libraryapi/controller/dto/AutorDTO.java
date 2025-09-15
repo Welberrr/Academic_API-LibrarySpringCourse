e9006e26314876f3a6ef8104716f79d474782dbc
Welberrr <welberhenrique.costa@gmail.com>
2025-09-15 15:13:30 -0300

    Feat: Tipos de validacao do Spring

--- src/main/java/io/github/welberrr/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/welberrr/libraryapi/controller/dto/AutorDTO.java
@@ -1,12 +1,11 @@
 package io.github.welberrr.libraryapi.controller.dto;
 
 import io.github.welberrr.libraryapi.model.Autor;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
-public record AutorDTO(java.util.UUID id, @NotBlank(message = "Campo Obrigatorio") String nome, @NotNull(message = "Campo Obrigatorio") LocalDate dataNascimento, @NotBlank(message = "Campo Obrigatorio") String nacionalidade) {
+public record AutorDTO(java.util.UUID id, @NotBlank(message = "Campo Obrigatorio") @Size(min = 2, max = 100, message = "Campo fora do tamanho padrao") String nome, @NotNull(message = "Campo Obrigatorio") @Past(message = "nao pode ser uma data futura") LocalDate dataNascimento, @NotBlank(message = "Campo Obrigatorio") @Size(max = 50, min = 2, message = "Campo fora do tamanho padrao") String nacionalidade) {
 
     public Autor mapearParaAutor(){
         Autor autor = new Autor();
