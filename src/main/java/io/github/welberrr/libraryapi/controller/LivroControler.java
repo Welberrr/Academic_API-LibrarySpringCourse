ccd628e381f76744995847293d0909dd22cf6876
Welberrr <welberhenrique.costa@gmail.com>
2025-09-17 23:00:09 -0300

    Feat: Endpoint para cadastro de livro com Bean Validation

--- src/main/java/io/github/welberrr/libraryapi/controller/LivroControler.java
+++ src/main/java/io/github/welberrr/libraryapi/controller/LivroControler.java
@@ -1,7 +1,14 @@
 package io.github.welberrr.libraryapi.controller;
 
+import io.github.welberrr.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.welberrr.libraryapi.controller.dto.ErroResposta;
+import io.github.welberrr.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.welberrr.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroControler {
 
     private final LivroService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto){
+        try {
+            
+
+        } catch (RegistroDuplicadoException e){
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
 }
--- src/main/java/io/github/welberrr/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/welberrr/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,10 +1,13 @@
 package io.github.welberrr.libraryapi.controller.dto;
 
 import io.github.welberrr.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record CadastroLivroDTO(String isbn, String titulo, LocalDate dataPublicacao, GeneroLivro genero, BigDecimal preco, UUID idAutor) {
+public record CadastroLivroDTO(@ISBN @NotBlank(message = "Campo Obrigatorio!") String isbn, @NotBlank(message = "Campo Obrigatorio!") String titulo, @NotNull(message = "Campo Obrigatorio!") LocalDate dataPublicacao, GeneroLivro genero, BigDecimal preco, @NotNull(message = "Campo Obrigatorio!") UUID idAutor) {
 }
