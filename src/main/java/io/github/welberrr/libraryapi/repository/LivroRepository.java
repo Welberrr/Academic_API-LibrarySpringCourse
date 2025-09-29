dc46c51b54d368c1e2a23d2d83185de0f3d1deb8
Welberrr <welberhenrique.costa@gmail.com>
2025-09-29 14:00:34 -0300

    Feat: Specification do Spring DataJPA

--- src/main/java/io/github/welberrr/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/welberrr/libraryapi/repository/LivroRepository.java
@@ -4,6 +4,7 @@ import io.github.welberrr.libraryapi.model.Autor;
 import io.github.welberrr.libraryapi.model.GeneroLivro;
 import io.github.welberrr.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface LivroRepository extends JpaRepository<Livro, UUID> {
+public interface LivroRepository extends JpaRepository<Livro, UUID>, JpaSpecificationExecutor<Livro> {
 
     List<Livro> findByAuto(Autor autor);
     List<Livro> findByTitulo(String titulo);
--- src/main/java/io/github/welberrr/libraryapi/service/LivroService.java
+++ src/main/java/io/github/welberrr/libraryapi/service/LivroService.java
@@ -1,11 +1,14 @@
 package io.github.welberrr.libraryapi.service;
 
+import io.github.welberrr.libraryapi.model.GeneroLivro;
 import io.github.welberrr.libraryapi.model.Livro;
 import io.github.welberrr.libraryapi.repository.LivroRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.expression.spel.ast.OpInc;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -25,5 +28,10 @@ public class LivroService {
 
     public void deletar(Livro livro){
         repository.delete(livro);
+    }
+
+    public List<Livro> pesquisa(String isbn, String titulo, String nome, GeneroLivro genero, Integer anoPublicacao){
+        Specification<Livro> specs = null;
+        return repository.findAll(specs);
     }
 }
