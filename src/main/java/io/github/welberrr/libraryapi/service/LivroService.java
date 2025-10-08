75b1f8c821e4e1fcaec86e4ffbee04aaaccc171c
Welberrr <welberhenrique.costa@gmail.com>
2025-10-07 21:32:55 -0300

    Feat: Specifications Livros

--- src/main/java/io/github/welberrr/libraryapi/service/LivroService.java
+++ src/main/java/io/github/welberrr/libraryapi/service/LivroService.java
@@ -3,6 +3,7 @@ package io.github.welberrr.libraryapi.service;
 import io.github.welberrr.libraryapi.model.GeneroLivro;
 import io.github.welberrr.libraryapi.model.Livro;
 import io.github.welberrr.libraryapi.repository.LivroRepository;
+import io.github.welberrr.libraryapi.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.expression.spel.ast.OpInc;
@@ -31,7 +32,23 @@ public class LivroService {
     }
 
     public List<Livro> pesquisa(String isbn, String titulo, String nome, GeneroLivro genero, Integer anoPublicacao){
-        Specification<Livro> specs = null;
-        return repository.findAll(specs);
+
+        Specification<Livro> specs = Specification.where(LivroSpecs.isbnEqual(isbn).and(LivroSpecs.tituloLike(titulo)).and(LivroSpecs.generoEqual(genero)));
+
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction() );
+
+        if (isbn != null){
+            specs = specs.and(LivroSpecs.isbnEqual(isbn));
+        }
+
+        if (titulo != null){
+            specs = specs.and(LivroSpecs.tituloLike(titulo));
+        }
+
+        if (genero != null){
+            specs = specs.and(LivroSpecs.generoEqual(genero));
+        }
+
+        return repository.findAll(LivroSpecs.isbnEqual(isbn));
     }
 }
